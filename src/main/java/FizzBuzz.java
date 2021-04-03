0de127971c4172668e4c327683fb74892edc9162
Ravel Andrade <ravel.gou@gmail.com>
2021-04-03 15:24:53 -0300

    TestAdvancedFizzBuzz green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,21 +1,20 @@
 public class FizzBuzz {
     public static String getValues() {
         String values = new String();
-        for(int i = 1; i<=3; i++) {
+        for(int i = 1; i<=13; i++) {
             values = values.concat(getValue(i));
         }
         return values;
     }
 
     private static String getValue(int i) {
-
-        if(i%3==0) {
+        Integer value = i;
+        if(i%3==0 || value.toString().contains("3")) {
             return "Fizz\n";
         }
-        if(i%5==0){
+        if(i%5==0|| value.toString().contains("5")){
             return "Buzz\n";
         }
-        Integer value = i;
         return value.toString()+"\n";
     }
 
--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -2,14 +2,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestFizzBuzz {
-    @Test
-    public void testBasicFizzBuzz(){
-        Assertions.assertEquals(FizzBuzz.getValues(), "1\n2\nFizz\n");
-    }
-
+    
     @Test
     public void testAdvancedFizzBuzz(){
         Assertions.assertEquals(FizzBuzz.getValues(), "1\n2\nFizz\n4\nBuzz\nFizz\n7" +
-                "\n8\nFizz\nBuzz\n11\nFizz\nFizz");
+                "\n8\nFizz\nBuzz\n11\nFizz\nFizz\n");
     }
 }
