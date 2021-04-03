c77d5098be7c862fbbbe0e1d411ec2cd50bef2e1
Ravel Andrade <ravel.gou@gmail.com>
2021-04-03 15:07:47 -0300

    TestFizzBuzz refactored

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,17 @@
 public class FizzBuzz {
     public static String getValues() {
-        return "1\n2\nFizz";
+        String values = new String();
+        for(int i = 1; i<=3; i++){
+            if(i%3==0){
+                values = values.concat("Fizz\n");
+            }else{
+                if(i%5==0){
+                    values = values.concat("Buzz\n");
+                }else{
+                    values = values.concat(i+"\n");
+                }
+            }
+        }
+        return values;
     }
 }
--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.Test;
 public class TestFizzBuzz {
     @Test
     public void testFizzBuzz(){
-        Assertions.assertEquals(FizzBuzz.getValues(), "1\n2\nFizz");
+        Assertions.assertEquals(FizzBuzz.getValues(), "1\n2\nFizz\n");
     }
 }
